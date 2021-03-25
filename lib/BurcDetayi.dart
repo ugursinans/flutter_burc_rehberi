4c1abcd969aeb9d965aabcad436d0bf047dfc762
s.ugursinan@gmail.com <U1g2u3r4>
2021-03-25 18:23:21 +0300

    BurcDetayı Sayfası App Bar için Palette Generator Eklendi.

--- lib/BurcDetayi.dart
+++ lib/BurcDetayi.dart
@@ -1,9 +1,32 @@
 import 'package:burc_rehberi/BurcListesi.dart';
 import 'package:flutter/material.dart';
+import 'package:palette_generator/palette_generator.dart';
 
-class BurcDetayi extends StatelessWidget {
+class BurcDetayi extends StatefulWidget {
   int index;
   BurcDetayi(this.index);
+  @override
+  _BurcDetayiState createState() => _BurcDetayiState();
+}
+
+class _BurcDetayiState extends State<BurcDetayi> {
+  PaletteGenerator value;
+  Color baskinRenk;
+
+  getPaletteGenerator() async {
+    value = await PaletteGenerator.fromImageProvider(
+        AssetImage('images/' + BurcListesi.data[widget.index].burcBuyukResim));
+    setState(() {
+      baskinRenk = value.dominantColor.color;
+      debugPrint(baskinRenk.toString());
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    getPaletteGenerator();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,18 +37,19 @@ class BurcDetayi extends StatelessWidget {
           expandedHeight: 200,
           primary: true,
           pinned: true,
+          backgroundColor: baskinRenk == null ? Colors.indigo : baskinRenk,
           flexibleSpace: FlexibleSpaceBar(
             centerTitle: true,
             title: Text('Burc Detayı'),
             background: Image.asset(
-              'images/' + BurcListesi.data[index].burcBuyukResim,
+              'images/' + BurcListesi.data[widget.index].burcBuyukResim,
               fit: BoxFit.cover,
             ),
           ),
         ),
         SliverToBoxAdapter(
           child: Text(
-            BurcListesi.data[index].burcDetayi,
+            BurcListesi.data[widget.index].burcDetayi,
             style: TextStyle(color: Colors.black, fontSize: 14),
           ),
         )
